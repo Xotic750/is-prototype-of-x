195334b3950c572d35f55cfca0042a9609e74eb4
Graham Fairweather <xotic750@gmail.com>
2019-07-16 14:53:19 +0200

    :bookmark: v2.0.2
    Auto build

--- dist/is-prototype-of-x.js
+++ dist/is-prototype-of-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-16T11:18:33.726Z",
+  "date": "2019-07-16T12:53:14.030Z",
   "describe": "",
   "description": "Checks if an object exists in another object's prototype chain.",
   "file": "is-prototype-of-x.js",
   "hash": "8a337558322c92462ca7",
   "license": "MIT",
-  "version": "2.0.1"
+  "version": "2.0.2"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
