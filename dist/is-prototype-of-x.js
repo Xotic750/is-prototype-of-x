d949de41f3f2bb0ec18a3bbbb018529c82052b0f
Graham Fairweather <xotic750@gmail.com>
2019-08-08 00:36:02 +0200

    Lodash security fix

--- dist/is-prototype-of-x.js
+++ dist/is-prototype-of-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-07T01:40:45.424Z",
+  "date": "2019-08-07T22:35:57.138Z",
   "describe": "",
   "description": "Checks if an object exists in another object's prototype chain.",
   "file": "is-prototype-of-x.js",
